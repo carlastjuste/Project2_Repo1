0afd44dde1848abb9f6082be15eb115fba139042
abissier <71886314+abissier@users.noreply.github.com>
2021-02-11 19:01:18 -0800

    fxed connection js file

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-
+// bring in the models and routes
 var db = require('./models');
-require('./routes/api-routes')(app);
-require('./routes/html-routes')(app);
+var routes = require('./controllers/recipes_controller');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static('public'));
 
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -22,12 +22,13 @@ app.engine(
   exphbs({
     defaultLayout: 'main',
     helpers: {
-      renderUrl: (value) => (value === '/recipes' ? true : false),
+      renderUrl: (value) => (value === '/burgers' ? true : false),
     },
   }),
 );
-
 app.set('view engine', 'handlebars');
+
+app.use(routes);
 
 db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
