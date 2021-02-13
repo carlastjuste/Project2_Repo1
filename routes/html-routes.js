8509e2e1aa9803e23a02e243967799273dd2f9af
Antonne Turner <antonneturner@gmail.com>
2021-02-13 14:23:09 -0500

    add myrecipes handlebar, add route for myrecipes, add db variable and import models table

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,4 +11,28 @@ module.exports = function (app) {
     app.get("/recipes", function (req, res) {
         res.render("recipes")
     });
+    app.get("/myrecipes", function (req, res) {
+        res.render("myrecipes")
+    });
 };
+
+var db = require("../models");
+
+router.get('/myrecipes', async (req, res) => {
+    try {
+        const dbRecipes = (await db.Recipe.findAll({
+
+        })).map((el) => el.get({ plain: true }));
+
+        // display recipes on recipes-search-result
+        const hbsObject = {
+            recipes: dbRecipes,
+        };
+        console.log(hbsObject)
+        return res.render('myrecipes', hbsObject);
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err);
+
+    }
+});
