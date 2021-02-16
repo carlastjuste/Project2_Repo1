c72a53ec3527dd69a7f4fbe2a288bc78387430ff
carlastjuste <carlastjuste@users.noreply.github.com>
2021-02-16 07:23:14 -0500

    adding some seed for Grocerylist

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,27 @@ INSERT INTO recipe (recipeName, ingredients, instructions, categoryType)
 VALUES
     ("Spaghetti", "1 box Spaghetti, 1 15 ounce can Tomato Sauce, Parmesan", "Cook spaghetti according to package directions.
 Spoon sauce over drained spaghetti noodles and sprinkle with parmesan cheese.", "Italian");
+
+
+  INSERT 
+  INTO GroceryLists 
+      (groceryListName
+       , createdAt
+       , updatedAt
+      )  
+   values
+     ('Walmart Shopping list'
+     , CURDATE()
+     , CURDATE()
+     );
+     INSERT 
+  INTO GroceryLists 
+      (groceryListName
+       , createdAt
+       , updatedAt
+      )  
+   values
+     ('Wholefood Shopping list'
+     , CURDATE()
+     , CURDATE()
+     );
