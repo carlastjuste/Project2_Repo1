5ac9b9efafbec1204533aad51db04af8db5250a1
abissier <71886314+abissier@users.noreply.github.com>
2021-02-16 16:34:38 -0800

    changed sendall function and recipe controller

--- server.js
+++ server.js
@@ -13,29 +13,28 @@ var RoutesGrocery = require('./controllers/grocery_controller');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'));
-
-app.use(RoutesRecipe);
-app.use(RoutesGrocery)
-
-app.get('/', (req, res) => res.render("index"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.engine(
   'handlebars',
   exphbs({
     defaultLayout: 'main',
-    helpers: {
-      renderUrl: (value) => (value === '/recipes' ? true : false),
-    },
-  }),
+    // helpers: {
+    //   renderUrl: (value) => (value === '/recipes' ? true : false),
+    // },
+  })
 );
 app.set('view engine', 'handlebars');
+
+
+app.use(RoutesRecipe);
+app.use(RoutesGrocery)
+app.get('/', (req, res) => res.render("index"));
 
 //Start the Server when the Db is ready to use
 db.sequelize.sync({ force: false }).then(function () {
